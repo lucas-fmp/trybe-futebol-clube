ded2436a0747e1d19cbe4f6a48471b72bed7b79e
Lucas Fernando <lucasfernandomacedo13@gmail.com>
2022-11-29 18:18:11 -0300

    Criação da Model e Migration de Teams

---
+++ app/backend/src/database/models/TeamModel.ts
@@ -0,0 +1,42 @@
+import { STRING, INTEGER, Model } from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class Team extends Model {
+  // declare <campo>: <tipo>;
+  declare id: number;
+  declare teamName: string;
+}
+
+Team.init({
+  // ... Campos
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: STRING(100),
+    allowNull: false,
+  },
+}, {
+  // ... Outras configs
+  underscored: true,
+  sequelize: db,
+  modelName: 'user',
+  timestamps: false,
+});
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+*/
+
+// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
+// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
+
+// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
+
+export default Team;
